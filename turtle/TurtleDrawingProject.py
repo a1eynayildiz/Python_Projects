312de8da6b770fb99be90fb364e7e23cff0d774d
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-11-24 13:15:22 +0300

    turtle ile uygulamalar yapıldı

--- turtle/TurtleDrawingProject.py
+++ turtle/TurtleDrawingProject.py
@@ -1,28 +1,43 @@
 import turtle
 
 drawing_board = turtle.Screen()
-drawing_board.bgcolor("deeppink")
+drawing_board.bgcolor("light pink")
 drawing_board.title("Python Turtle")
 
 turtle_instance = turtle.Turtle()
 
-# Kare çiz
-"""for i in range(4):
+#  Kare çiz
+''' for i in range(4):
     turtle_instance.forward(200)# = Kalemi bulunduğu yönde 100 birim ileri götür
     turtle_instance.right(90)#right(90) = Kalemi sağa doğru 90 derece döndür
-"""
+    turtle.done() # bunu demezsek ekran kapanmaz
+'''
 #yıldız
-import turtle
-
-turtle_instance = turtle.Turtle()
+'''
 
 for i in range(5):
+    turtle_instance.color("brown")
     turtle_instance.forward(200)
     turtle_instance.left(144)
 
 turtle.done()
+'''
 
+#iç içe kare çiz
+def shrinkingSquare(size):
+    for i in range(4):
+        turtle_instance.forward(size)
+        turtle_instance.left(90)
+        size = size - 5
 
-turtle.done() # bunu demezsek ekran kapanmaz
-
+shrinkingSquare(150)
+shrinkingSquare(130)
+shrinkingSquare(110)
+shrinkingSquare(90)
+shrinkingSquare(80)
+shrinkingSquare(60)
+shrinkingSquare(40)
+shrinkingSquare(20)
+shrinkingSquare(10)
+turtle.done()
 
