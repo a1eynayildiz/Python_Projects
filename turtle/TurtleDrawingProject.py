dca8975fea741f83d6994192ed74692c056bcca2
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-11-25 20:42:42 +0300

    catch the turtle project

--- turtle/TurtleDrawingProject.py
+++ turtle/TurtleDrawingProject.py
@@ -24,7 +24,7 @@ turtle.done()
 '''
 
 #iç içe kare çiz
-def shrinkingSquare(size):
+"""def shrinkingSquare(size):
     for i in range(4):
         turtle_instance.forward(size)
         turtle_instance.left(90)
@@ -38,6 +38,20 @@ shrinkingSquare(80)
 shrinkingSquare(60)
 shrinkingSquare(40)
 shrinkingSquare(20)
-shrinkingSquare(10)
+shrinkingSquare(10)"""
+
+
+#kalp çiz
+turtle_instance.speed(3)
+turtle_instance.color("red")
+turtle_instance.begin_fill()
+turtle_instance.left(140)
+turtle_instance.forward(200)
+turtle_instance.circle(-100,200) #yarıçap ve derece
+turtle_instance.left(120)
+turtle_instance.circle(-100,200)
+turtle_instance.forward(200)
+turtle_instance.end_fill()
+
 turtle.done()
 
